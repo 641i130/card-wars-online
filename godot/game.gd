18a1d5c72215a43c38d1e894a35d67de5d3a6df0
641i130 <galileoray@gmail.com>
2020-09-14 23:08:27 -0500

    Organized Folders, focusing more on godot because it can output to HTML5.

---
+++ godot/game.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Create sprite
+	#set_texture(value)
+	pass 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
