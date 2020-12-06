807cf3c2a7fe4ccac12e02e563e08fbbdf500575
641i130 <galileoray@gmail.com>
2020-12-06 00:19:00 -0600

    Added HP markup and exit=ESC button

--- godot/objects/game.gd
+++ godot/objects/game.gd
@@ -8,9 +8,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Create sprite
-	#set_texture(value)
-	pass 
+	set_process(true)
+
+
+# Exit game when press ESC for ease of debugging
+func _process(delta):
+	if Input.is_action_pressed("key_exit"):
+		get_tree().quit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- godot/objects/menu.gd
+++ godot/objects/menu.gd
@@ -14,7 +14,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+# Exit game when press ESC for ease of debugging
+func _process(delta):
+	if Input.is_action_pressed("key_exit"):
+		get_tree().quit()
 
 func _on_Button_pressed(): # startgame button
 	get_tree().change_scene("res://scenes/game.tscn") # when pressed, switch to the game 
