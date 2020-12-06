25e5eb130a78f31c98d4bd0c54d1780586dcb0e6
641i130 <galileoray@gmail.com>
2020-12-06 17:15:39 -0600

    Added card drawing function before the randomized card chooser for the players hand

--- godot/objects/Area2D.gd
+++ godot/objects/Area2D.gd
@@ -1,17 +1,26 @@
 extends Area2D
 
-onready var card = get_tree().get_root().get_node("Desk/Deck/Card")
-var rng = RandomNumberGenerator.new()
+# When loaded, set this variable to the  sprite in the player's hand
+onready var one = get_tree().get_root().get_node("Desk/Player Hand/One/One")
+onready var two = get_tree().get_root().get_node("Desk/Player Hand/Two/Two")
+onready var three = get_tree().get_root().get_node("Desk/Player Hand/Three/Three")
+onready var four = get_tree().get_root().get_node("Desk/Player Hand/Four/Four")
+onready var five = get_tree().get_root().get_node("Desk/Player Hand/Five/Five")
 
+onready var hand = [[0, one],[1, two], [2, three],[3, four],[4, five]]
+
+var rng = RandomNumberGenerator.new()
+onready var counter = 0;
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton \
-	and event.button_index == BUTTON_LEFT \
-	and event.is_pressed():
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
+		# If mouse click left button then give player a card      
 		print("Click")
 		random_card()
 		
 func random_card():
-	rng.randomize()
-	var rand_card = load("res://assets/cards/" +str(rng.randi_range(1, 512))+ ".jpg" )
-	card.set_texture(rand_card)
+	if counter <= 4:
+		rng.randomize()
+		var rand_card = load("res://assets/cards/" +str(rng.randi_range(1, 512))+ ".jpg" )
+		hand[counter][1].set_texture(rand_card)
+		counter+=1
